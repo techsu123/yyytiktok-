40a0faa96e971ab909681efcb3467cb00e29ab19
techsu123 <techsumana123@gmail.com>
2025-12-08 23:57:03 +0530

    Update webhook.py

--- api/webhook.py
+++ api/webhook.py
@@ -1,12 +1,12 @@
 import json
-from main import bot  # import your bot instance
+from app import app# import your bot instance
 from flask import Request, Response
 
 def handler(request: Request):
     try:
         data = request.get_json()
         if data:
-            bot.process_new_updates([data])
+            app.process_new_updates([data])
         return Response("ok", status=200)
     except Exception as e:
         return Response(str(e), status=500)
